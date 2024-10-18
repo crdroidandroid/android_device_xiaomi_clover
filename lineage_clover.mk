bdbad339aaa6fcc9b91a049506ca2cec3e3f9cd7
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    clover: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_clover.mk
+++ lineage_clover.mk
@@ -17,12 +17,11 @@ PRODUCT_MODEL := Mi PAD 4
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_CHARACTERISTICS := tablet
 
-PRODUCT_SYSTEM_NAME := clover
-
-BUILD_FINGERPRINT := "Xiaomi/clover/clover:8.1.0/OPM1.171019.019/V9.6.4.0.ODJCNFD:user/release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="clover-user 8.1.0 OPM1.171019.019 V9.6.4.0.ODJCNFD release-keys" \
-    TARGET_PRODUCT="clover"
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+    BuildDesc="clover-user 8.1.0 OPM1.171019.019 V9.6.4.0.ODJCNFD release-keys" \
+    BuildFingerprint=Xiaomi/clover/clover:8.1.0/OPM1.171019.019/V9.6.4.0.ODJCNFD:user/release-keys \
+    DeviceName=clover \
+    DeviceProduct=clover \
+    SystemName=clover
