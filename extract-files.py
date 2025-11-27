e3c818956be4628cfb473c61e952390e88769ff1
ShevT <evgeny.shishkov@gmail.com>
2025-11-27 11:21:36 +0300

    clover: Cleanup and update fingerprint blobs
     Update from clover-user 8.1.0 OPM1.171019.019 9.8.29 release-keys
    
    Change-Id: I2a65e1f997f4b300eea364711a10a19ef138aa02

--- extract-files.py
+++ extract-files.py
@@ -70,7 +70,6 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     (
-        'vendor/lib/vendor.qti.hardware.fingerprint@1.0.so',
         'vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so',
     ): blob_fixup()
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
